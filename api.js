32639f59ccecbd195f177a02af524ffa77190bb6
so5522325-commits <so5522325@gmail.com>
2026-07-01 00:54:17 +0530

    Update index.html

---
+++ api.js
@@ -0,0 +1,9 @@
+const { Client, Databases } = Appwrite;
+const client = new Client()
+    .setEndpoint('https://cloud.appwrite.io/v1') 
+    .setProject('6a413df70035d232adfb');
+
+export const databases = new Databases(client);
+export const DATABASE_ID = '6a414c58001cef943254';
+export const COLLECTION_ID = 'nexus_os_';
+
