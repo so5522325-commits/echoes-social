524586d0f9dad8f5b34444ea1b097d3967a116c5
so5522325-commits <so5522325@gmail.com>
2026-07-02 00:20:56 +0530

    Update index.html

--- api.js
+++ api.js
@@ -1,13 +1,8 @@
-import { Client, Databases, Account } from "https://cdn.jsdelivr.net/npm/appwrite@14.0.0/+esm";
-
-const client = new Client();
-client
-    .setEndpoint('https://fra.cloud.appwrite.io/v1') // Yahan apna Appwrite Endpoint dalein
-    .setProject('6a413df70035d232adfb');        // Yahan apna Project ID dalein
-
+import { Client, Account, Databases, Storage } from "https://cdn.jsdelivr.net/npm/appwrite@14.0.0/+esm";
+const client = new Client().setEndpoint('https://cloud.appwrite.io/v1').setProject('6a413df70035d232adfb');
 export const account = new Account(client);
 export const databases = new Databases(client);
-
-// Apni IDs yahan se export karein taaki app.js mein mil sakein
+export const storage = new Storage(client);
+export const clientInstance = client;
 export const DATABASE_ID = '6a414c58001cef943254';
 export const COLLECTION_ID = 'nexus_os_';
